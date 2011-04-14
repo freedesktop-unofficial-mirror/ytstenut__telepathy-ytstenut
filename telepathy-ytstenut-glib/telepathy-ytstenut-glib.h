605136af96ca6fd7f85baccd3536f34e58daed57
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-04-14 09:53:31 +0100

    tp-ytstenut-glib.h: also include status.h
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- telepathy-ytstenut-glib/telepathy-ytstenut-glib.h
+++ telepathy-ytstenut-glib/telepathy-ytstenut-glib.h
@@ -9,7 +9,7 @@
 #include "telepathy-ytstenut-glib/channel-factory.h"
 #include "telepathy-ytstenut-glib/channel.h"
 #include "telepathy-ytstenut-glib/client.h"
-
+#include "telepathy-ytstenut-glib/status.h"
 
 #endif /* TP_YTSTENUT_GLIB_H */
 
